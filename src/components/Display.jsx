71394a5873891927f048922faf82c1660a15fb72
royce <royakhabhoa@gmail.com>
2025-02-20 16:00:09 +0100

    ...

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -14,7 +14,7 @@ export default function Display({ values }) {
         readOnly
         style={{ fontFamily: "digital-clock-font" }}
         className="cursor-default w-9/12 focus:outline-none rounded-lg 
-        mb-3 h-15 bg-white p-4 border-black text-right text-4xl font-bold text-black"
+        mb-3 h-15 bg-white p-4 border-black text-right text-2xl font-bold text-black"
       />
     </div>
   );
